a53e1caef4f45942cccc9a265c7d76bc66e26892
willsaibott <willsaibott@gmail.com>
2020-04-10 11:46:16 -0300

    fixing usage details of enum_factory

--- AdvancedLibraryCpp/enum_factory.h
+++ AdvancedLibraryCpp/enum_factory.h
@@ -4,38 +4,48 @@ namespace advanced {
 
   /**
    * Usage:
+   * In dummy.h:
+   * #include <algorithm>
    * enum class dummy {
    *  first,
    *  second,
    *  third,
    *  ...
    *  MAX
-   * }
+   * };
    *
    * namespace std {
    *
    *  inline string
    *  to_string(const dummy& d) {
-   *    static std::string names[static_cast<dummy::MAX>] = {
+   *    static std::string names[static_cast<size_t>(dummy::MAX)+1ul] = {
    *      "first",
    *      "second",
    *      "third",
    *      ...
-   *      return names[static_cast<size_t>(d)];
+   *       "MAX"
+   *      };
+   *      return names[std::min(static_cast<size_t>(d), static_cast<size_t>(dummy::MAX))];
    *    }
    *  }
    * }
+   * ...
    *
+   * in main.cpp:
+   * #include "dummy.h"
+   * #include "enum_factory.h"
    * int main() {
-   *     dummy t = from_string("second"); // dummy::second;
+   *     const dummy d  = advanced::from_string<dummy>("second"); // dummy::second;
+   *     const dummy d2 = advanced::from_string<dummy, dummy::first, dummy::first>("second"); // first
    * }
+   *
    */
   template <class T, T minimum = static_cast<T>(0), T maximum = T::MAX>
   inline T
   from_string(const std::string& str) {
-    T result{T::MAX};
-    for (T value = static_cast<T>(0);
-           value < T::MAX;
+    T result{ maximum };
+    for (T value = minimum;
+           value < maximum;
            value = static_cast<T>(static_cast<size_t>(value)+1))
     {
       if (str == std::to_string(value)) {

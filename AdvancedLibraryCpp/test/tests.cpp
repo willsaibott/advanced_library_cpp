eb8fdb1fb5e2a5ce5f40384441c032917ab711f5
willsaibott <willsaibott@gmail.com>
2020-05-06 14:02:06 -0300

    Adding a main version that works with QT test scanner

--- AdvancedLibraryCpp/test/tests.cpp
+++ AdvancedLibraryCpp/test/tests.cpp
@@ -18,6 +18,229 @@
 
 namespace fs = std::experimental::filesystem;
 
+/*
+ *
+  #define DECLARE_TEST(TestClass) \
+  {\
+      TESTLIB_SELFCOVERAGE_START(#TestClass) \
+      QCoreApplication app(argc, argv); \
+      app.setAttribute(Qt::AA_Use96Dpi, true); \
+      TestClass test; \
+      QTEST_SET_MAIN_SOURCE_PATH \
+      QString path { "results/" + test.objectName() + ".xml" }; \
+      auto status = QTest::qExec(&test, argc, argv); \
+      if (status) { \
+        std::cerr << "Test " << suite << " returned: " << status << std::endl; \
+      } \
+  }\
+*/
+
+#define TESTGUI
+#ifdef TESTGUI
+
+int main(int argc, char** argv) {
+  fs::create_directory("results");
+  size_t suite{ 0 };
+
+  // if (true) to become collapsable
+
+  if (true)
+  {
+      TESTLIB_SELFCOVERAGE_START(TestLockable)
+      QCoreApplication app(argc, argv);
+      app.setAttribute(Qt::AA_Use96Dpi, true);
+      TestLockable test;
+      QTEST_SET_MAIN_SOURCE_PATH \
+      QString path { "results/" + test.objectName() + ".xml" };
+      auto status = QTest::qExec(&test, argc, argv);
+      if (status) {
+        std::cerr << "Test " << suite << " returned: " << status << std::endl;
+      }
+  }
+  if (true)
+  {
+    TESTLIB_SELFCOVERAGE_START(TestEnum)
+        QCoreApplication app(argc, argv);
+    app.setAttribute(Qt::AA_Use96Dpi, true);
+    TestEnum test;
+    QTEST_SET_MAIN_SOURCE_PATH \
+        QString path { "results/" + test.objectName() + ".xml" };
+    auto status = QTest::qExec(&test, argc, argv);
+    if (status) {
+      std::cerr << "Test " << suite << " returned: " << status << std::endl;
+    }
+  }
+  if (true)
+  {
+    TESTLIB_SELFCOVERAGE_START(TestMemory)
+        QCoreApplication app(argc, argv);
+    app.setAttribute(Qt::AA_Use96Dpi, true);
+    TestMemory test;
+    QTEST_SET_MAIN_SOURCE_PATH \
+        QString path { "results/" + test.objectName() + ".xml" };
+    auto status = QTest::qExec(&test, argc, argv);
+    if (status) {
+      std::cerr << "Test " << suite << " returned: " << status << std::endl;
+    }
+  }
+  if (true)
+  {
+    TESTLIB_SELFCOVERAGE_START(TestStreamDelimiters)
+        QCoreApplication app(argc, argv);
+    app.setAttribute(Qt::AA_Use96Dpi, true);
+    TestStreamDelimiters test;
+    QTEST_SET_MAIN_SOURCE_PATH \
+        QString path { "results/" + test.objectName() + ".xml" };
+    auto status = QTest::qExec(&test, argc, argv);
+    if (status) {
+      std::cerr << "Test " << suite << " returned: " << status << std::endl;
+    }
+  }
+  if (true)
+  {
+    TESTLIB_SELFCOVERAGE_START(TestRandom)
+        QCoreApplication app(argc, argv);
+    app.setAttribute(Qt::AA_Use96Dpi, true);
+    TestRandom test;
+    QTEST_SET_MAIN_SOURCE_PATH \
+        QString path { "results/" + test.objectName() + ".xml" };
+    auto status = QTest::qExec(&test, argc, argv);
+    if (status) {
+      std::cerr << "Test " << suite << " returned: " << status << std::endl;
+    }
+  }
+  if (true)
+  {
+    TESTLIB_SELFCOVERAGE_START(TestMissingElements)
+        QCoreApplication app(argc, argv);
+    app.setAttribute(Qt::AA_Use96Dpi, true);
+    TestMissingElements test;
+    QTEST_SET_MAIN_SOURCE_PATH \
+        QString path { "results/" + test.objectName() + ".xml" };
+    auto status = QTest::qExec(&test, argc, argv);
+    if (status) {
+      std::cerr << "Test " << suite << " returned: " << status << std::endl;
+    }
+  }
+  if (true)
+  {
+    TESTLIB_SELFCOVERAGE_START(TestSubset)
+        QCoreApplication app(argc, argv);
+    app.setAttribute(Qt::AA_Use96Dpi, true);
+    TestSubset test;
+    QTEST_SET_MAIN_SOURCE_PATH \
+        QString path { "results/" + test.objectName() + ".xml" };
+    auto status = QTest::qExec(&test, argc, argv);
+    if (status) {
+      std::cerr << "Test " << suite << " returned: " << status << std::endl;
+    }
+  }
+  if (true)
+  {
+    TESTLIB_SELFCOVERAGE_START(TestSubArray)
+        QCoreApplication app(argc, argv);
+    app.setAttribute(Qt::AA_Use96Dpi, true);
+    TestSubArray test;
+    QTEST_SET_MAIN_SOURCE_PATH \
+        QString path { "results/" + test.objectName() + ".xml" };
+    auto status = QTest::qExec(&test, argc, argv);
+    if (status) {
+      std::cerr << "Test " << suite << " returned: " << status << std::endl;
+    }
+  }
+  if (true)
+  {
+    TESTLIB_SELFCOVERAGE_START(TestWrapperThread)
+        QCoreApplication app(argc, argv);
+    app.setAttribute(Qt::AA_Use96Dpi, true);
+    TestWrapperThread test;
+    QTEST_SET_MAIN_SOURCE_PATH \
+        QString path { "results/" + test.objectName() + ".xml" };
+    auto status = QTest::qExec(&test, argc, argv);
+    if (status) {
+      std::cerr << "Test " << suite << " returned: " << status << std::endl;
+    }
+  }
+  {
+    TESTLIB_SELFCOVERAGE_START(TestMessageQueue)
+        QCoreApplication app(argc, argv);
+    app.setAttribute(Qt::AA_Use96Dpi, true);
+    TestMessageQueue test;
+    QTEST_SET_MAIN_SOURCE_PATH \
+        QString path { "results/" + test.objectName() + ".xml" };
+    auto status = QTest::qExec(&test, argc, argv);
+    if (status) {
+      std::cerr << "Test " << suite << " returned: " << status << std::endl;
+    }
+  }
+  if (true)
+  {
+    TESTLIB_SELFCOVERAGE_START(TestLRUCache)
+        QCoreApplication app(argc, argv);
+    app.setAttribute(Qt::AA_Use96Dpi, true);
+    TestLRUCache test;
+    QTEST_SET_MAIN_SOURCE_PATH \
+        QString path { "results/" + test.objectName() + ".xml" };
+    auto status = QTest::qExec(&test, argc, argv);
+    if (status) {
+      std::cerr << "Test " << suite << " returned: " << status << std::endl;
+    }
+  }
+  if (true)
+  {
+    TESTLIB_SELFCOVERAGE_START(TestSemaphore)
+        QCoreApplication app(argc, argv);
+    app.setAttribute(Qt::AA_Use96Dpi, true);
+    TestSemaphore test;
+    QTEST_SET_MAIN_SOURCE_PATH \
+        QString path { "results/" + test.objectName() + ".xml" };
+    auto status = QTest::qExec(&test, argc, argv);
+    if (status) {
+      std::cerr << "Test " << suite << " returned: " << status << std::endl;
+    }
+  }
+  if (true)
+  {
+    TESTLIB_SELFCOVERAGE_START(TestTimer)
+        QCoreApplication app(argc, argv);
+    app.setAttribute(Qt::AA_Use96Dpi, true);
+    TestTimer test;
+    QTEST_SET_MAIN_SOURCE_PATH \
+        QString path { "results/" + test.objectName() + ".xml" };
+    auto status = QTest::qExec(&test, argc, argv);
+    if (status) {
+      std::cerr << "Test " << suite << " returned: " << status << std::endl;
+    }
+  }
+  if (true)
+  {
+    TESTLIB_SELFCOVERAGE_START(TestBinaryTree)
+        QCoreApplication app(argc, argv);
+    app.setAttribute(Qt::AA_Use96Dpi, true);
+    TestBinaryTree test;
+    QTEST_SET_MAIN_SOURCE_PATH \
+        QString path { "results/" + test.objectName() + ".xml" };
+    auto status = QTest::qExec(&test, argc, argv);
+    if (status) {
+      std::cerr << "Test " << suite << " returned: " << status << std::endl;
+    }
+  }
+
+// Qt does not recognize under macros:  :(
+//  DECLARE_TEST(TestRandom)
+//  DECLARE_TEST(TestMissingElements)
+//  DECLARE_TEST(TestSubset)
+//  DECLARE_TEST(TestSubArray)
+//  DECLARE_TEST(TestWrapperThread)
+//  DECLARE_TEST(TestMessageQueue)
+//  DECLARE_TEST(TestLRUCache)
+//  DECLARE_TEST(TestSemaphore)
+//  DECLARE_TEST(TestTimer)
+//  DECLARE_TEST(TestBinaryTree)
+}
+
+#else
+
 int
 main(int argc, char** argv) {
   Q_UNUSED(argc);
@@ -56,3 +279,5 @@ main(int argc, char** argv) {
 
   return final_status;
 }
+
+#endif
